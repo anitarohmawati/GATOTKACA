24a1e18f1c41918eb7e59095429a3633fc6e69d9
Anita Rohmawati <anita.rohmawati@gmail.com>
2022-11-09 21:43:33 +0700

    tes

--- intro.py
+++ intro.py
@@ -1,10 +1,15 @@
 import streamlit as st
 import pandas as pd
-st.write("Anita")
-x=st.slider("select anumber")
-st.write("You selected",x)
-df=pd.read_excel("https://github.com/anitarohmawati/GATOTKACA/blob/ANITA/mydata.xlsx?raw=true")
-st.dataframe(df)
-st.line_chart(df)
 
 
+
+def main() : 
+    st.write("Predicting Best Renewable Energy Investment on Sub-Saharan Countries for Electrification Acceleration")
+    x=st.slider("select anumber")
+    st.write("You selected",x)
+    df=pd.read_excel("https://github.com/anitarohmawati/GATOTKACA/blob/ANITA/mydata.xlsx?raw=true")
+    st.dataframe(df)
+    st.line_chart(df)
+
+if __name__ == '__main__' : 
+    main()
