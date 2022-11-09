7d343a97e1961b4ca7a67b0f77d683a22bc8b796
Anita Rohmawati <anita.rohmawati@gmail.com>
2022-11-09 20:58:07 +0700

    test

--- intro.py
+++ intro.py
@@ -1,17 +1,8 @@
-import streamlit as st 
-import requests
+import streamlit as st
+import pandas as pd
+st.write("Anita")
+x=st.slider("select anumber")
+st.write("You selected",x)
+df=pd.read_excel("https://github.com/anitarohmawati/GATOTKACA/blob/ANITA/mydata.xlsx?raw=true")
+st.line_chart(df)
 
-def main() : 
-  st.header('This is Header')
-  st.subheader('This is SubHeader')
-  st.markdown('# Rendering Markdown ')
-  st.write('Some Phytagorean Equation : ')
-  st.latex('c^2 = a^2+b^2')
-
-from st_aggrid import AgGrid
-#baca dataframe dari file csv 
-titanic = pd.read_csv('https://raw.githubusercontent.com/mofdac/-materi-das/main/01.%20Python%20for%20DA/titanic.csv')
-#read json file dari data covid 
-st.dataframe(titanic)
-if __name__ == '__main__' : 
-  main()
