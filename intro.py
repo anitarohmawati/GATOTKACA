25723d5f31c3972884f1824f1f6ea2e1e1a50ce6
Anita Rohmawati <anita.rohmawati@gmail.com>
2022-11-09 17:58:17 +0700

    tes

--- intro.py
+++ intro.py
@@ -3,6 +3,6 @@ import pandas as pd
 st.write("Anita")
 x=st.slider("select anumber")
 st.write("You selected",x)
-df=pd.read_excel("mydata.xlsx")
+df=pd.read_excel("https://github.com/anitarohmawati/GATOTKACA/blob/ANITA/mydata.xlsx")
 st.line_chart(df)
 
