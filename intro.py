cc0abbaee241e7517d8d64c95873ed592ffe7943
Anita Rohmawati <anita.rohmawati@gmail.com>
2022-11-09 17:40:15 +0700

    tes

--- intro.py
+++ intro.py
@@ -14,7 +14,7 @@ def main() :
   st.write("Metrics")
   st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
   st.write("Menampilkan Dataframe dengan St AgGrid")
-  AgGrid(titanic)
+
   st.table([x for x in range(1,5)])
 if __name__ == "__main__" : 
   main()
