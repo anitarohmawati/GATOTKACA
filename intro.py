70c21d3ac90c84e5e4cb3b7f86f6291559713539
Anita Rohmawati <anita.rohmawati@gmail.com>
2022-11-09 17:40:03 +0700

    test

--- intro.py
+++ intro.py
@@ -2,23 +2,19 @@ import streamlit as st
 import pandas as pd
 st.write("Anita")
 
-import streamlit as st 
-import pandas as pd 
-import requests
-from st_aggrid import AgGrid
-#baca dataframe dari file csv 
+
 titanic = pd.read_csv("https://raw.githubusercontent.com/mofdac/-materi-das/main/01.%20Python%20for%20DA/titanic.csv")
 #read json file dari data covid 
 
 
 def main() : 
-  st.write('Contoh dataframe')
+  st.write("Contoh dataframe")
   st.dataframe(titanic)
 
-  st.write('Metrics')
+  st.write("Metrics")
   st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
-  st.write('Menampilkan Dataframe dengan St AgGrid')
+  st.write("Menampilkan Dataframe dengan St AgGrid")
   AgGrid(titanic)
   st.table([x for x in range(1,5)])
-if __name__ == '__main__' : 
+if __name__ == "__main__" : 
   main()
