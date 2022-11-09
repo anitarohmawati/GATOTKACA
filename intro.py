d5ee1ce94e7ed2f6d7f96d97ef5a81e3e17b2765
Anita Rohmawati <anita.rohmawati@gmail.com>
2022-11-09 23:12:20 +0700

    tes

--- intro.py
+++ intro.py
@@ -21,6 +21,9 @@ def load_data_SSA():
 def main() : 
    
     st.title("Predicting Best Reneweable Energy Investment for Electrification Acceleration in Sub-Saharan Africa Rurals")
-    st.altair_chart(load_data_SSA())
+    c = alt.Chart(load_data_SSA()).mark_circle().encode(
+    x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+
+    st.altair_chart(c, use_container_width=True)
 if __name__ == '__main__' : 
     main()
