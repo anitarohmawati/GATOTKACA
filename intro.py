d218163ce51b9c31874bcbf789e6edccda59be03
Anita Rohmawati <anita.rohmawati@gmail.com>
2022-11-09 17:36:38 +0700

    read excel

--- intro.py
+++ intro.py
@@ -2,5 +2,5 @@ import streamlit as st
 import pandas as pd
 st.write("Anita")
 
-data=pd.read_excel("IRENA_RE_Public_Investment_July2022.xlsx").copy()
+data=pd.read_excel("https://github.com/anitarohmawati/GATOTKACA/blob/ANITA/IRENA_RE_Public_Investment_July2022.xlsx").copy()
 st.dataframe(data)
