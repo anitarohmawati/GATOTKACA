65a0ec143348678037600477f0491ecbf6ff3084
Anita Rohmawati <anita.rohmawati@gmail.com>
2022-11-09 20:48:49 +0700

    tes

--- intro.py
+++ intro.py
@@ -1,4 +1,5 @@
 import streamlit as st 
+import requests
 
 def main() : 
   st.header('This is Header')
@@ -7,5 +8,10 @@ def main() :
   st.write('Some Phytagorean Equation : ')
   st.latex('c^2 = a^2+b^2')
 
+from st_aggrid import AgGrid
+#baca dataframe dari file csv 
+titanic = pd.read_csv('https://raw.githubusercontent.com/mofdac/-materi-das/main/01.%20Python%20for%20DA/titanic.csv')
+#read json file dari data covid 
+st.dataframe(titanic)
 if __name__ == '__main__' : 
   main()
