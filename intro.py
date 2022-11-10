b50d35ac46d6c142e446150811cb2868a335a7e6
Anita Rohmawati <anita.rohmawati@gmail.com>
2022-11-10 13:26:44 +0700

    tes

--- intro.py
+++ intro.py
@@ -15,11 +15,15 @@ st.set_page_config(layout="wide", page_title="Renewable Energy Investment", page
 def load_data_SSA():
     data = pd.read_excel(
         "SSH Rural Access to Electricity 2020.xlsx"
-        # set as datetime instead of converting after the fact
     )
-    data=data[["Country Name", 2020]]
-    return data
+    
+    data = data[data[2020] < 50].sort_values(by=2020, ascending=True)
+    return 
 
+def get_optimize(target, final_df):
+    delta = (final_df.prediction - target).abs().nsmallest(5)
+    opt = final_df.iloc[delta.index.to_list()]
+    return opt.iloc[:,1:-1].reset_index(drop=True)
 
 def main() : 
     sns.set_theme(style="whitegrid")
@@ -28,7 +32,7 @@ def main() :
     st.markdown("#### In 2020, there were 40 countries in SSA whose rural area were still below 50% in electricity access as follows:")
     df=load_data_SSA()
     #st.dataframe(df)
-   
+    reference_df = pd.read_csv('reference_df.csv')
    
   
     data_africa=pd.read_excel("SSH Rural Access to Electricity 2020.xlsx")
@@ -61,6 +65,28 @@ def main() :
     tab1, tab2 = st.tabs(['Background','Optimizer'])
     tab1.subheader("The Needs of Electification on Sub-Sahara Africa")
     tab2.subheader("Optimizing Renewable Energy Investment for Electrification Acceleration in Sub-Saharan Africa Region")
+
+    with tab1:
+        #columns :
+        col1, col2 = st.columns(2)
+        with col1:
+            st.markdown("#### In 2020, there were countries in SSA whose rural area were still below 50% in electricity access as follows:")
+            st.markdown("#### Imagine the positive impacts we can accrue should the donor from developed countries position their investment to promote the electrification acceleration on those areas!")
+
+        with col2:
+            st.dataframe(df)
+    with tab2:
+        col1, col2= st.columns([2,4])
+        with col1:
+            #Input (Typing)
+            num_input = st.number_input('Insert the Target of Electrification Growth', min_value=0.0, max_value=4.5)
+            click_me_btn = st.button('Click Me')
+        with col2:
+            if click_me_btn:
+                if num_input > 0.0:
+                    st.dataframe(get_optimize(num_input, reference_df))
+                else:
+                    st.write('Input Error')
 
 if __name__ == '__main__' : 
     main()
