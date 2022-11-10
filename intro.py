5ee59a65e0ce6aec1a46bcacd4c90696e192df11
Anita Rohmawati <anita.rohmawati@gmail.com>
2022-11-10 13:36:38 +0700

    test

--- intro.py
+++ intro.py
@@ -16,7 +16,7 @@ def load_data_SSA():
     data = pd.read_excel(
         "SSH Rural Access to Electricity 2020.xlsx"
     )
-    
+    data=data[["Country Name",2020]]
     data = data[data[2020] < 50].sort_values(by=2020, ascending=True)
     return 
 
