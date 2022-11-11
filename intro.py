4f709ea0637855cbe1a0dfd8cf1f19aac6888dfb
Anita Rohmawati <anita.rohmawati@gmail.com>
2022-11-11 08:35:46 +0700

    tes

--- intro.py
+++ intro.py
@@ -60,7 +60,22 @@ def load_data(url, filename, file_type):
         return file_type(f)
 
 def main() : 
+    df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2011_us_ag_exports.csv')
 
+    fig = go.Figure(data=go.Choropleth(
+    locations=df['code'], # Spatial coordinates
+    z = df['total exports'].astype(float), # Data to be color-coded
+    locationmode = 'USA-states', # set of locations match entries in `locations`
+    colorscale = 'Reds',
+    colorbar_title = "Millions USD",
+    ))
+
+    fig.update_layout(
+    title_text = '2011 US Agriculture Exports by State',
+    geo_scope='usa', # limite map scope to USA
+    )
+
+    fig.show()
 
 
     sns.set_theme(style="whitegrid")
